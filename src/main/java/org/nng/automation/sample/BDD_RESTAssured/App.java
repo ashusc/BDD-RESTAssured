23204a9843c16050ae087aeae517e1e91cfc0f81
Ashutosh Mishra <35286920+ashusc@users.noreply.github.com>
2018-12-05 17:10:58 +0530

    Project structure created

---
+++ src/main/java/org/nng/automation/sample/BDD_RESTAssured/App.java
@@ -0,0 +1,13 @@
+package org.nng.automation.sample.BDD_RESTAssured;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
